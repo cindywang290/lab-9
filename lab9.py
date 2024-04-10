7b2aaee811e0b2fa86b07ae2befeed99973b94a6
cindy <wang.cindy@ufl.edu>
2024-04-10 17:45:25 -0400

    Cindy's code without decoder

---
+++ lab9.py
@@ -0,0 +1,33 @@
+def main():
+    while True:
+        print('''Menu
+-------------
+1. Encode
+2. Decode
+3. Quit''')
+        selection = input('Please enter an option: ')
+        if selection == '1':
+            original_password = input('Please enter your password to encode: ')
+            encoded_password = encoder(original_password)
+            print("Your password has been encoded and stored!\n")
+            continue
+        if selection == '2':
+            print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+        if selection == '3':
+            exit()
+
+def encoder(password):
+    s = ''
+    for digit in (password):
+        encoded_digit = str(int(digit) + 3)
+        s += encoded_digit
+    return s
+
+# def decoder(password):
+#     s = ''
+#     for digit in (password):
+#         encoded_digit = str(int(digit) - 3)
+#         s += encoded_digit
+#     return s
+#
+# main()
